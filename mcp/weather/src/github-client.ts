ae6108325fd5d2d1ac249912be791bb7bc2b207a
wenjunche <wenjun@openfin.co>
2025-06-22 21:32:22 -0400

    mcp

--- mcp/weather/src/github-client.ts
+++ mcp/weather/src/github-client.ts
@@ -9,16 +9,18 @@ import OpenAI from 'openai';
 const apiKey = process.env.openaiApiKey; 
 const githubToken = process.env.githubToken;
 
+const mcpServerUrl = 'http://localhost:3002/mcp'; // 'https://api.githubcopilot.com/mcp/';
+
 async function main() {
 
-  const transport = new StreamableHTTPClientTransport(
-    new URL('https://api.githubcopilot.com/mcp/'), {
-        requestInit: {
-            headers: {
-              'Authorization': `Bearer ${githubToken}`,
-                // 'Content-Type': 'application/json',
-            }
-        }
+const transport = new StreamableHTTPClientTransport(
+    new URL(mcpServerUrl), {
+        // requestInit: {
+        //     headers: {
+        //       'Authorization': `Bearer ${githubToken}`,
+        //         // 'Content-Type': 'application/json',
+        //     }
+        // }
       }
 );
 
@@ -43,8 +45,6 @@ const client = new OpenAI({
 //       version: '1.0.0',
 //   });
 
-const mcpServerUrl = 'https://api.githubcopilot.com/mcp/';
-
 try {
     // await mcpClient.connect(transport);
     // console.log("Connected using Streamable HTTP transport", transport.sessionId);
@@ -55,33 +55,35 @@ try {
     });
     // console.log("Available tools:", JSON.stringify(tools, null, 2));
 
-    const { text: answer } = await generateText({
-      model: openai('gpt-4o-mini'),
-      tools,
-      maxSteps: 10,
-      onStepFinish: async ({ toolResults }) => {
-        console.log(`STEP RESULTS: ${JSON.stringify(toolResults, null, 2)}`);
-      },
-      system: 'You are an AI assistant that can interact with GitHub. Use the available tools to answer questions about GitHub repositories and users.',
-      // prompt: 'please create an issue in repository named azure-ai, created by wenjunche, with description "this is a test issue created by AI"',
-      prompt: 'please list all the issues in repository named azure-ai, created by wenjunche',
-    });
-    console.log(`FINAL ANSWER: ${answer}`);
-
-    // const response = await client.responses.create({
-    //     model:  'gpt-4o-mini',
-    //     input:  'please create an issue in repository named azure-ai, created by wenjunche, with description "this is a test issue created by AI"',
-    //     tools: [{
-    //         type: 'mcp',
-    //         server_url: mcpServerUrl,
-    //         server_label: 'github-MCP-Server',
-    //         require_approval: 'never',
-    //         headers: {
-    //             'Authorization': `Bearer ${githubToken}`,
-    //         }
-    //     }]
+    // const { text: answer } = await generateText({
+    //   model: openai('gpt-4o-mini'),
+    //   tools,
+    //   maxSteps: 10,
+    //   onStepFinish: async ({ toolResults }) => {
+    //     console.log(`STEP RESULTS: ${JSON.stringify(toolResults, null, 2)}`);
+    //   },
+    //   system: 'You are an AI assistant that can interact with GitHub. Use the available tools to answer questions about GitHub repositories and users.',
+    //   // prompt: 'please create an issue in repository named azure-ai, created by wenjunche, with description "this is a test issue created by AI"',
+    //   // prompt: 'please get all issues in repository named azure-ai, created by wenjunche, and search for issues that mention "Mazy" in the title or body',
+    //   prompt: 'what is the weather alert in NY today?',
     // });
-    // console.log('AI returned', response);
+    // console.log(`FINAL ANSWER: ${answer}`);
+
+    const response = await client.responses.create({
+         model:  'gpt-4o-mini',
+    //     input:  'please create an issue in repository named azure-ai, created by wenjunche, with description "this is a test issue created by AI"',
+         input: 'please get all issues in repository named azure-ai, created by wenjunche, and search for issues that mention "Mazy" in the title or body',
+         tools: [{
+             type: 'mcp',
+             server_url: mcpServerUrl,
+             server_label: 'github-MCP-Server',
+             require_approval: 'never',
+             headers: {
+                 'Authorization': `Bearer ${githubToken}`,
+             }
+         }]
+     });
+    console.log('AI returned', response);
 
   } catch (error) {
     console.error('Error:', error);
